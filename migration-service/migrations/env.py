6a84ae3ff8398fa9914ec65170ae499b3f239be5
Ian <47256454+IanRFerguson@users.noreply.github.com>
2025-08-27 06:21:50 -0400

    add alembic service

--- migration-service/migrations/env.py
+++ migration-service/migrations/env.py
@@ -8,7 +8,7 @@ from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import create_engine, text
-from utilities.models.database import db
+from utilities.models import db
 
 ####
 
@@ -18,16 +18,16 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+prod_run = os.environ.get("STAGE") == "prod"
 target_metadata = db.Model.metadata
-sqlalchemy_url = (
-    "postgresql://{user}:{password}@{host}:{port}/{dbname}?sslmode=require".format(
-        user=os.environ["DB_USERNAME"],
-        password=os.environ["DB_PASSWORD"],
-        host=os.environ["DB_HOST"],
-        port=int(os.environ["DB_PORT"]),
-        dbname=os.environ["DB_DATABASE"],
-    )
+sqlalchemy_url = "postgresql://{user}:{password}@{host}:{port}/{dbname}".format(
+    user=os.environ["DB_USERNAME"] if prod_run else os.environ["DEV_DB_USERNAME"],
+    password=os.environ["DB_PASSWORD"] if prod_run else os.environ["DEV_DB_PASSWORD"],
+    host=os.environ["DB_HOST"] if prod_run else "localhost",
+    port=int(os.environ["DB_PORT"]) if prod_run else 5432,
+    dbname=os.environ["DB_DATABASE"] if prod_run else os.environ["DEV_DB_DATABASE"],
 )
+print(sqlalchemy_url)
 
 
 def run_migrations_offline() -> None:
@@ -55,20 +55,32 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
+def include_name(name, type_, parent_names):
+    if type_ == "schema":
+        return name == "tmc_dev"
+    return True
+
+
 def run_migrations_online() -> None:
     """
     Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
-    This version sets the search_path to a specific schema.
+    This version sets the search_path and explicitly includes the schema.
     """
 
     connectable = create_engine(sqlalchemy_url)
 
     with connectable.begin() as connection:
         connection.execute(text("SET search_path TO tmc_dev"))
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            version_table_schema="tmc_dev",
+            include_schemas=True,
+            include_name=include_name,
+        )
 
         with context.begin_transaction():
             context.run_migrations()
